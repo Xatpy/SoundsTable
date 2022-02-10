51f6584c2736c858c1ca8513bcbb7dd3a283f08d
Jaime <jaime.chapinal@gmail.com>
2022-02-10 19:53:49 +0100

    Force image and sw

--- ElXokas/serviceWorker.js
+++ ElXokas/serviceWorker.js
@@ -7,7 +7,12 @@ const assets = [
   "./aaa-mis-ojos.mp3",
   "./a-estudiar.mp3",
   "https://raw.githubusercontent.com/Xatpy/SoundsTable/master/ElXokas/a-ver-si-se-oye-asi-bien.mp3",
-  "https://raw.githubusercontent.com/Xatpy/SoundsTable/master/ElXokas/bueno.mp3"
+  "https://raw.githubusercontent.com/Xatpy/SoundsTable/master/ElXokas/bueno.mp3",
+  "https://raw.githubusercontent.com/Xatpy/SoundsTable/master/ElXokas/calla-la-boca.mp3",
+  "https://raw.githubusercontent.com/Xatpy/SoundsTable/master/ElXokas/callate.mp3",
+  "https://raw.githubusercontent.com/Xatpy/SoundsTable/master/ElXokas/como-que-un-juego.mp3",
+  "https://raw.githubusercontent.com/Xatpy/SoundsTable/master/ElXokas/de-verdad.mp3",
+  "https://raw.githubusercontent.com/Xatpy/SoundsTable/master/ElXokas/dos-por-uno-en-burros.mp3"
 ]
 
 self.addEventListener("install", installEvent => {
